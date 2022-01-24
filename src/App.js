415f7659a714a960d1a6dd46de3863be8a555489
skaiste pakalniskyte <skaiste.sky@gmail.com>
2022-01-24 22:51:41 +0000

    animated background

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import React, { useState, useContext } from 'react';
 import Card from './Components/Card';
 import StartQuiz from './Components/StartQuiz';
 import './index.css';
-
 import { QuizContext } from './Helpers/Contexts';
 
 export default function App() {
 	const [gameState, setGameState] = useState("start");
 
 	return (
+
 		<div className="App">
 		<h1>Quiz App</h1>
         <QuizContext.Provider value={{ gameState, setGameState }}>
         {gameState === "start" && <StartQuiz />}
 		{gameState === "card" && <Card />}
         </QuizContext.Provider>
+		<div class="slider-thumb"></div>
+		<div class="slider-thumb1"></div>
+		<div class="slider-thumb2"></div>
 		</div>
-		
 	);
 }
