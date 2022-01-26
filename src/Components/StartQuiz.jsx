1f86d58efd3e6761638127303467b6fe5daea072
skaiste pakalniskyte <skaiste.sky@gmail.com>
2022-01-26 15:10:39 +0000

    background

--- src/Components/StartQuiz.jsx
+++ src/Components/StartQuiz.jsx
@@ -7,6 +7,7 @@ function StartQuiz() {
 
 	return (
 	<div className="Start">
+	<p>Be smarter, quicker and brainier!<br/>Put your quiz skills to the test and broaden your knowledge.</p>
     <button onClick={() => {setGameState("card")}}>Start Quiz</button>
 	</div>
 	);
