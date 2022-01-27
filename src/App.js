33da13424412375485f61bb11f73e1431cc78643
Matt <matt@MJMain.localdomain>
2022-01-27 18:06:37 +0000

    btn change

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
 // Using a react hook 'useContext'
-import React, { useState, useContext } from 'react';
-import Card from './Components/Card';
-import StartQuiz from './Components/StartQuiz';
-import './index.css';
-import { QuizContext } from './Helpers/Contexts';
+import React, { useState, useContext } from "react";
+import Card from "./Components/Card";
+import StartQuiz from "./Components/StartQuiz";
+import "./index.css";
+import { QuizContext } from "./Helpers/Contexts";
 
 export default function App() {
-	const [gameState, setGameState] = useState("start");
-	const [quizOptions, setQuizOptions] = useState({
-		gameState: "start",
-		difficulty: "easy",
-		category: "linux",
-		number: 5,
-	  })
+  const [gameState, setGameState] = useState("start");
+  const [quizOptions, setQuizOptions] = useState({
+    gameState: "start",
+    difficulty: "easy",
+    category: "linux",
+    number: 5,
+  });
 
-	const saveOptionsDataHandler = (enteredOptions) => {
-		setQuizOptions({
-			...enteredOptions
-		})
-	}
+  const saveOptionsDataHandler = (enteredOptions) => {
+    setQuizOptions({
+      ...enteredOptions,
+    });
+  };
 
-	const saveGameStateRestart = (receivedData) => {
-		setGameState(receivedData)
-	}
+//   const saveGameStateRestart = (receivedData) => {
+//     setGameState({receivedData});
+// 	console.log(gameState)
+//   };
 
-	return (
-
-		<div className="App">
-		<h1>Quiz App</h1>
-        <QuizContext.Provider value={{ gameState, setGameState }}>
-        {quizOptions.gameState === "start" && <StartQuiz onSaveQuizOptions={saveOptionsDataHandler}/>}
-		{quizOptions.gameState === "card" && <Card quizOptions={quizOptions} restartFunction={saveGameStateRestart}/>}
-        </QuizContext.Provider>
-		<div className="slider-thumb"></div>
-		<div className="slider-thumb1"></div>
-		<div className="slider-thumb2"></div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <h1>Quiz App</h1>
+      <QuizContext.Provider value={{ gameState, setGameState }}>
+        {quizOptions.gameState === "start" && (
+          <StartQuiz onSaveQuizOptions={saveOptionsDataHandler} />
+        )}
+        {quizOptions.gameState === "card" && (
+          <Card
+            quizOptions={quizOptions}
+          />
+        )}
+      </QuizContext.Provider>
+      <div className="slider-thumb"></div>
+      <div className="slider-thumb1"></div>
+      <div className="slider-thumb2"></div>
+    </div>
+  );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,84 +1,93 @@
 import React, { useState, useEffect } from "react";
 import baseURL from "./Request";
 import axios from "axios";
+import "../index.css";
 
 export default function Card(props) {
-	const [isLoading, setIsLoading] = useState(true);
-	const [questions, setQuestions] = useState([]);
-	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const [showScore, setShowScore] = useState(false);
-	const [score, setScore] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
 
-	
-	let URL = `${baseURL}&category=${props.quizOptions.content}&difficulty=${props.quizOptions.difficulty}&limit=${props.quizOptions.number}`;
-	useEffect(() => {
-	async function fetchData() {
-		console.log(props)
-		const request = await axios.get(`${URL}`);
-		console.log(URL)
-		setQuestions(request.data);
-		setIsLoading(false);
-		return request;
-	}
-	fetchData();
-	}, [URL]);
+  let URL = `${baseURL}&category=${props.quizOptions.content}&difficulty=${props.quizOptions.difficulty}&limit=${props.quizOptions.number}`;
+  useEffect(() => {
+    async function fetchData() {
+      console.log(props);
+      const request = await axios.get(`${URL}`);
+      console.log(URL);
+      setQuestions(request.data);
+      setIsLoading(false);
+      return request;
+    }
+    fetchData();
+  }, [URL]);
 
-	const handleAnswerOptionClick = (isCorrect) => {
-	if (isCorrect === questions[currentQuestion].correct_answer) {
-		setScore(score + 1);
-	}
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect === questions[currentQuestion].correct_answer) {
+      setScore(score + 1);
+    }
 
-	const nextQuestion = currentQuestion + 1;
-	if (nextQuestion < questions.length) {
-		setCurrentQuestion(nextQuestion);
-	} else {
-		setShowScore(true);
-	}
-	};
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
 
-	const questionContent = isLoading ? (
-	<div>...Loading</div>
-	) : (
-	<>
-		<div className="question-section">
-		<div className="question-count">
-			<span>Question {currentQuestion + 1}</span>/{questions.length}
-		</div>
-		<div className="question-text">
-			{questions[currentQuestion].question}
-		</div>
-		</div>
-		<div className="answer-section">
-		{Object.keys(questions[currentQuestion].answers).map((answerOption) =>
-			questions[currentQuestion].answers[answerOption] === null ? (
-			<div></div>
-			) : (
-			<button onClick={() => handleAnswerOptionClick(answerOption)}>
-				{questions[currentQuestion].answers[answerOption]}
-			</button>
-		)
-		)}
-		</div>
-	</>
-	);
+//   const resetHandler = (event) => {
+//     event.preventDefault();
+//     props.restartFunction(event);
+//   };
 
-	return (
-	<div className="app">
-		{showScore ? (
-		<div className="score-section">
-			You scored {score} out of {questions.length}
-		</div>
-		) : (
-		<div>{questionContent}</div>
-		)}
+  const questionContent = isLoading ? (
+    <div>...Loading</div>
+  ) : (
+    <>
+      <div className="question-section">
+        <div className="question-count">
+          <span>Question {currentQuestion + 1}</span>/{questions.length}
+        </div>
+        <div className="question-text">
+          {questions[currentQuestion].question}
+        </div>
+      </div>
+      <div className="answer-section">
+        {Object.keys(questions[currentQuestion].answers).map((answerOption) =>
+          questions[currentQuestion].answers[answerOption] === null ? (
+            <div></div>
+          ) : (
+            <button onClick={() => handleAnswerOptionClick(answerOption)}>
+              {questions[currentQuestion].answers[answerOption]}
+            </button>
+          )
+        )}
+      </div>
+    </>
+  );
 
-<button type="button" onClick={ props.restartFunction("start") }>
-    <span>Restart</span>
-  </button>
-
-
-
-
-	</div>
-	);
+  return (
+    <div>
+      <div className="app">
+        {showScore ? (
+          <div className="score-section">
+            You scored {score} out of {questions.length}
+          </div>
+        ) : (
+          <div>{questionContent}</div>
+        )}
+      </div>
+      {/* <div className="btnDiv">
+        <button
+          className="btn"
+          type="button"
+		  value={"start"}
+          onClick={resetHandler}
+        >
+          Restart
+        </button>
+      </div> */}
+    </div>
+  );
 }
